fa3c633da5d0e169eeac94074d8db49b84d88c61
Ray Cha <ray@weatherpattern.com>
2018-01-24 21:28:22 -0500

    refactored code into functions

--- ia_healthcheck.py
+++ ia_healthcheck.py
@@ -1,22 +1,53 @@
 import requests
 
+def query_webmondb():
+	links = [
+		'epa.gov',
+		'energy.gov',
+		'doi.gov',
+		'usda.gov',
+		'noaa.gov'
+	]	
+	return links
 
-fileoutput = open("ia_healthcheck.txt","w")
+def query_wayback(url):
+	url = 'http://archive.org/wayback/available?url=%s' % url
+	response = requests.get(url)
+	response = response.json()
+	return response
 
-links = ['http://archive.org/wayback/available?url=epa.gov',
-	'http://archive.org/wayback/available?url=energy.gov',
-	'http://archive.org/wayback/available?url=doi.gov',
-	'http://archive.org/wayback/available?url=usda.gov',
-	'http://archive.org/wayback/available?url=noaa.gov']
+def get_time(response):
+	time = response['archived_snapshots']['closest']['timestamp']
 
-for url in links:
+	print(time)
+	return time
 
-	
-	ia_info = requests.get(url)
-	print(ia_info.content)
+def check_time(time):
+	return status
 
-	fileoutput.write('\n URL: \n')
-	fileoutput.write(str(ia_info.content))
-	
+def is_healthy(responses):
+	link_health = [get_time(response) for response in responses]
+	output_file(link_health)
+	return	
 
-fileoutput.close()
+def output_file(responses):
+	fileoutput = open("ia_healthcheck.txt", "w")
+
+	for url in responses:
+		fileoutput.write('\n URL: \n')
+		fileoutput.write(str(url))		
+
+	fileoutput.close()
+
+	return
+
+def output_email():
+	return
+
+links = query_webmondb()
+responses = [query_wayback(url) for url in links]
+is_healthy(responses)
+
+
+
+
